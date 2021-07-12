e4a890e3b3882d1db2fe62986f9222768ba0ee63
Andreas Fankhauser <https://hiddenalpha.ch/slnk/id/1:c0n74ct>
2021-07-12 14:51:26 +0200

    [SDCISA-6014] Revert more

--- src/test/java/jssc/SerialNativeInterfaceTest.java
+++ src/test/java/jssc/SerialNativeInterfaceTest.java
@@ -2,7 +2,9 @@ package jssc;
 
 import org.junit.Test;
 
-import static org.hamcrest.CoreMatchers.*;
+import static org.hamcrest.CoreMatchers.is;
+import static org.hamcrest.CoreMatchers.not;
+import static org.hamcrest.CoreMatchers.nullValue;
 import static org.junit.Assert.assertThat;
 import static org.junit.Assert.fail;
 
